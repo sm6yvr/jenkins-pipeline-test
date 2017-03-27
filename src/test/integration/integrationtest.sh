a664f8d3a82465c6f297b9d2f5a2eb9ccd5d077f
Roberth Andersson <roberth.andersson@vehco.com>
2017-03-27 09:03:49 +0200

    Update

--- src/test/integration/integrationtest.sh
+++ src/test/integration/integrationtest.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 
-function quit {
-	docker-compose stop
-	docker-compose rm -f
-	exit $1
-}
-
-docker-compose --verbose up -d
-
-
-# Make sure containers are ready for the test
-echo "Waiting for container to start"
-sleep 20
-
 first=$(curl -i -silent -X GET  192.168.1.139:8980/ | grep "HTTP/1.1")
+
+curl -i -silent -X GET  192.168.1.139:8980/ | grep "HTTP/1.1"
 
 status_first=$(echo "$first" | cut -f 2 -d ' ')
 
